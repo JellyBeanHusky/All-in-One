5679afbe55976c6dd2b6e4aeffdd5e18629dc0af
jacksaonmizta <46801905+jacksaonmizta@users.noreply.github.com>
2019-03-09 14:06:39 +0800

    Create uniAdminDb.php

---
+++ uniAdminDb.php
@@ -0,0 +1,34 @@
+<?php
+
+session_start();
+header('location:registeruni.php');
+
+$con = mysqli_connect('localhost','root','');
+
+mysqli_select_db($con, 'allinone');
+
+
+
+$name = $_POST['musername'];
+$pass = $_POST['mpassword'];
+$fname = $_POST['mfullname'];
+$mail = $_POST['memail'];
+$usertype=$_POST['usertype'];
+
+$s = "select *from users where name='$name'";
+
+$result = mysqli_query($con, $s);
+
+$num =mysqli_num_rows($result);
+
+if($num == 1){
+	echo" username already taken";
+}else{
+	
+	$reg= "insert into users(username, password, fullname, email, type, idno, mobileNo, birth, usertype) 
+	values('$name', '$pass', '$fname', '$mail', '$type', '$idno', '$mobile', '$dbirth', '$usertype' )";
+	mysqli_query($con, $reg);
+	echo" Registeration Successful";
+}
+
+?>
