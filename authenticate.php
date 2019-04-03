ef93be82fe1a56b19449a09add0ca4e4cc86cc37
jacksaonmizta <46801905+jacksaonmizta@users.noreply.github.com>
2019-04-04 00:37:50 +0800

    Update authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -1,26 +1,44 @@
 <?php
 
 
-session_start();
 
 $con = mysqli_connect('localhost','root','');
 
-mysqli_select_db($con, 'allinone');
+if(!$con)
+{
+	echo"Unable to establish connection".mysqli_error();
+}
+	$db=mysqli_select_db($con, "allinone");
+if(!$db)
+{
+	echo"Database not found". mysqli_error();
+}
 
-$name= $_POST['user'];
-$pass= $_POST['pwd'];
 
-$s ="select * from users where username='$name' && password='$pass'";
 
-$result = mysqli_query($con,$s);
-
-$num =mysqli_num_rows($result);
-
-if($num == 1){
+if(isset($_POST['submit'])){
 	
-	header('location: sasAdminHome.php');
-}else{
-	header('location: login.php');
+	$usertype=$_POST['usertype'];
+	$username=$_POST['user'];
+	$password=$_POST['pwd'];
+	
+	$query="select * from users where username='$username' and password='$password' and usertype='$usertype'";
+	$result=mysqli_query($con,$query);
+	
+	
+	
+	while($row=mysqli_fetch_array($result, MYSQLI_ASSOC)){
+	if($row['username']==$username && $row['password']==$password && $row['usertype']=='Admin'){
+		
+		header("Location: sasAdminHome.php");
+	}elseif($row['username']==$username && $row['password']==$password && $row['usertype']=='uniAdmin'){
+		header("Location: uniAdminHome.php");
+	}elseif($row['username']==$username && $row['password']==$password && $row['usertype']=='applicant'){
+		header("Location: userHome.php");
+	}else {
+			header("Location: login.php");		
+                }
+ }
 }
 
 ?>
