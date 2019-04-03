013a64c2aee6106bbc350f1588a9eca1d78d72e7
jellybeanhusky <kchandrasekara3@gmail.com>
2019-04-04 00:56:05 +0800

    Create reviewApplicationModel.php

---
+++ reviewApplicationModel.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once("inc/dbcall.php");
+$db = new Db();
+$sessionID = $_POST['sessionid'];
+//    var dataString = 'sessionid=' + id + '&udate=' + udate + '&utime=' + utime + '&ufee=' + ufee + '&uclassStatus=' + uclassStatus + '&uclassType=' + uclassType;
+$sql = "UPDATE `registered` SET "
+        . "`status` = '{$_POST['ustatus']}' "
+        .
+        " WHERE `registered`.`id` =" . $sessionID;
+$result = $db->query($sql);
+$row = mysqli_fetch_assoc($result);
+echo $sql;
