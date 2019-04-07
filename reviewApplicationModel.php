f8da1ef45e62f252a3c99042182e02b0aa19de44
jellybeanhusky <kchandrasekara3@gmail.com>
2019-04-08 01:32:51 +0800

    Update reviewApplicationModel.php

--- reviewApplicationModel.php
+++ reviewApplicationModel.php
@@ -3,7 +3,6 @@
 require_once("inc/dbcall.php");
 $db = new Db();
 $sessionID = $_POST['sessionid'];
-//    var dataString = 'sessionid=' + id + '&udate=' + udate + '&utime=' + utime + '&ufee=' + ufee + '&uclassStatus=' + uclassStatus + '&uclassType=' + uclassType;
 $sql = "UPDATE `registered` SET "
         . "`astatus` = '{$_POST['ustatus']}' "
         .
