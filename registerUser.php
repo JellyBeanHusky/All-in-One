de39ce3edc41e403a100f31bfd27a9cd9f2dce13
jellybeanhusky <kchandrasekara3@gmail.com>
2019-04-11 22:25:46 +0800

    Update registerUser.php

--- registerUser.php
+++ registerUser.php
@@ -2,24 +2,21 @@
 
 require_once("inc/dbcall.php");
 $db = new Db();
-//    var dataString = 'userid=' + userid + '&sessionid=' + sessionid;
 
 $userid = $_SESSION['uniqueID'];
 $sessionId = $_POST['sessionid'];
-//$query="select * from tsessions where status=1";
-//$uniname = $_SESSION['uniname'];
 
-//$row = mysqli_fetch_assoc($result);
-//$sql = "SELECT * FROM `registered` WHERE `userid`=" . $userid . " AND`sessionid` =" . $sessionId;
-//$result = $db->query($sql);
-//$numrows = $db->numRows($result);
-//if ($numrows > 0) {
-//    echo "Already Registered!";
-//} else {
-    //end of member data
+
+$sql = "SELECT * FROM `registered` WHERE `userid`=" . $userid . " AND`sessionid` =" . $sessionId;
+$result = $db->query($sql);
+$numrows = $db->numRows($result);
+if ($numrows > 0) {
+  echo "Already Applied!";
+} else {
+
 $sql = "INSERT INTO `registered` (`userid`,`sessionid`, `name`)
 VALUES ($userid,$sessionId,'{$_SESSION['username']}')";
 
 $db->query($sql);
-echo "Successfully Registered";
-//}
+echo "Application successful!";
+}
